56b95540962b141bc2662517efaf289e1d90a25d
Sigabort42 <elbenkrizi@gmail.com>
2018-02-07 15:00:18 +0100

    ft_ls

--- ft_liste.c
+++ ft_liste.c
@@ -12,45 +12,43 @@
 
 #include "includes/ft_ls.h"
 
-static char		*ft_law_file(mode_t st_mode, char *law)
+static void		ft_law_file(mode_t st_mode, char **law)
 {
+	*law = (char*)malloc(11);
 	if (S_ISDIR(st_mode))
-		law[0] = 'd';
+		(*law)[0] = 'd';
 	else if (S_ISFIFO(st_mode))
-		law[0] = 'p';
+		(*law)[0] = 'p';
 	else if (S_ISCHR(st_mode))
-		law[0] = 'c';
+		(*law)[0] = 'c';
 	else if (S_ISBLK(st_mode))
-		law[0] = 'b';
+		(*law)[0] = 'b';
 	else if (S_ISLNK(st_mode))
-		law[0] = 'l';
+		(*law)[0] = 'l';
 	else if (S_ISSOCK(st_mode))
-		law[0] = 's';
+		(*law)[0] = 's';
 	else
-		law[0] = '-';
-	law[1] = (st_mode & S_IRUSR) ? 'r' : '-';
-	law[2] = (st_mode & S_IWUSR) ? 'w' : '-';
-	law[3] = (st_mode & S_IXUSR) ? 'x' : '-';
-	law[4] = (st_mode & S_IRGRP) ? 'r' : '-';
-	law[5] = (st_mode & S_IWGRP) ? 'w' : '-';
-	law[6] = (st_mode & S_IXGRP) ? 'x' : '-';
-	law[7] = (st_mode & S_IROTH) ? 'r' : '-';
-	law[8] = (st_mode & S_IWOTH) ? 'w' : '-';
-	law[9] = (st_mode & S_IXOTH) ? 'x' : '-';
-	law[10] = 0;
-	return (law);
+		(*law)[0] = '-';
+	(*law)[1] = (st_mode & S_IRUSR) ? 'r' : '-';
+	(*law)[2] = (st_mode & S_IWUSR) ? 'w' : '-';
+	(*law)[3] = (st_mode & S_IXUSR) ? 'x' : '-';
+	(*law)[4] = (st_mode & S_IRGRP) ? 'r' : '-';
+	(*law)[5] = (st_mode & S_IWGRP) ? 'w' : '-';
+	(*law)[6] = (st_mode & S_IXGRP) ? 'x' : '-';
+	(*law)[7] = (st_mode & S_IROTH) ? 'r' : '-';
+	(*law)[8] = (st_mode & S_IWOTH) ? 'w' : '-';
+	(*law)[9] = (st_mode & S_IXOTH) ? 'x' : '-';
+	(*law)[10] = 0;
 }
 
 t_liste			*ft_listenew(t_env *env, struct dirent *dr)
 {
 	t_liste		*new;
-	char		*law;
 	char		*path_tmp;
 
-	law = (char*)malloc(11);
 	if (!(new = (t_liste*)malloc(sizeof(*new))) || !env)
 		return (0);
-	new->law = ft_law_file(env->s.st_mode, law);
+	ft_law_file(env->s.st_mode, &new->law);
 	new->name_root = ft_strdup(env->pass->pw_name);
 	new->group = ft_strdup(env->grp->gr_name);
 	new->size_lnk = env->s.st_nlink;
@@ -59,7 +57,7 @@ t_liste			*ft_listenew(t_env *env, struct dirent *dr)
 	new->date = ctime(&env->s.st_mtime);
 	new->date[24] = 0;
 	new->path_name = ft_strdup(dr->d_name);
-	if (law[0] == 'l')
+	if (new->law[0] == 'l')
 	{
 		new->path_name_link = ft_strnew(50);
 		path_tmp = ft_strnew(255);
