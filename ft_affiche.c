3ad1f3777f95b31cf65dbad5224569a71cc9c744
Elharag BENKRIZI <elbenkri@e1r1p5.42.fr>
2018-02-17 19:26:57 +0100

    ft_ls

--- ft_affiche.c
+++ ft_affiche.c
@@ -6,11 +6,28 @@
 /*   By: elbenkri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 13:03:45 by elbenkri          #+#    #+#             */
-/*   Updated: 2018/02/17 19:19:29 by elbenkri         ###   ########.fr       */
+/*   Updated: 2018/02/17 19:24:36 by elbenkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_ls.h"
+
+static void     ft_affiche3(t_env *env, t_liste *tmp)
+{
+	if ((tmp->path_name[0] != '.' && tmp->law[0] == 'l') && 
+	(env->flags & (1 << 1)))
+	{
+		ft_putstr(tmp->path_name);
+		write(1, " -> ", 4);
+		ft_putstr(tmp->path_name_link);
+		write(1, "\n", 1);
+	}
+	else if (tmp->path_name[0] != '.')
+	{
+		ft_putstr(tmp->path_name);
+		write(1, "\n", 1);
+	}
+}
 
 static void     ft_affiche2(t_env *env, int tri)
 {
@@ -35,21 +52,7 @@ static void     ft_affiche2(t_env *env, int tri)
 		else if (env->flags & (1 << 0) && tmp->path_name[0] == '.')
 			ft_flags_a(tmp);
 		else
-		{
-			if ((tmp->path_name[0] != '.' && tmp->law[0] == 'l') && 
-			(env->flags & (1 << 1)))
-			{
-				ft_putstr(tmp->path_name);
-				write(1, " -> ", 4);
-				ft_putstr(tmp->path_name_link);
-				write(1, "\n", 1);
-			}
-			else if (tmp->path_name[0] != '.')
-			{
-				ft_putstr(tmp->path_name);
-				write(1, "\n", 1);
-			}
-		}
+			ft_affiche3(env, tmp);
 		tmp = (tri == 0) ? tmp->next : tmp->prev;
 	}
 }
